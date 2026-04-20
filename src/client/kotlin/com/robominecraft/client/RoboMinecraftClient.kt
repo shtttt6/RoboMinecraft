75dd51d27b095ab04fa2af6e80789207a47b5544
shtttt <125881988+shtttt6@users.noreply.github.com>
2026-04-20 13:32:33 +0800

    Initial commit

---
+++ src/client/kotlin/com/robominecraft/client/RoboMinecraftClient.kt
@@ -0,0 +1,9 @@
+package com.robominecraft.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object RoboMinecraftClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/com/robominecraft/client/RoboMinecraftDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.robominecraft.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object RoboMinecraftDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
